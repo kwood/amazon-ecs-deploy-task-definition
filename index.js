8b46527e8e899a0357138e15d530803842249f89
Kevin Wood <kevin@kwood.me>
2023-01-02 23:22:04 -0600

    actually compile the thing

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function updateEcsService(ecs, clusterName, service, taskDefArn, waitForSe
 
   const consoleHostname = aws.config.region.startsWith('cn') ? 'console.amazonaws.cn' : 'console.aws.amazon.com';
 
-  core.info(`Deployment started. Watch this deployment's progress in the Amazon ECS console: https://${consoleHostname}/ecs/home?region=${aws.config.region}#/clusters/${clusterName}/services/${service}/events`);
+  core.info(`Deployment startedz. Watch this deployment's progress in the Amazon ECS console: https://${consoleHostname}/ecs/home?region=${aws.config.region}#/clusters/${clusterName}/services/${service}/events`);
 
   // Wait for service stability
   if (waitForService && waitForService.toLowerCase() === 'true') {
